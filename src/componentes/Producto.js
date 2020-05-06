7a9a2d89d69c1bcebf4d9291c9090a950e58e17b
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2020-05-06 07:57:43 -0500

    Se muestra el producto

--- src/componentes/Producto.js
+++ src/componentes/Producto.js
@@ -1,11 +1,16 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react'
+import '../css/productos.css'
 
 export class Producto extends Component {
     render() {
+        const {imagen,nombre,precio}=this.props.producto;
         return (
-            <div>
-                Hola desde producto {this.props.producto.id}
-            </div>
+            <li>
+                <img src={`img/${imagen}.png`} alt={nombre}/>
+                <p>{nombre} <span>$ {precio}</span></p>
+                <a href="#">Más Información</a>
+            </li>
         )
     }
 }
