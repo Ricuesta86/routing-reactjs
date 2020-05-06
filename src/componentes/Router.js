2386a9dc34924fd75114cdf4d3c552cc207ef7c5
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2020-05-06 15:15:24 -0500

    Se creo el componente de un solo producto

--- src/componentes/Router.js
+++ src/componentes/Router.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from './Header'
 import Productos from "./Productos";
 import Nosotros from "./Nosotros";
 import Error from "./Error";
 import infoProducto from '../datos/datos.json'
+import SingleProducto from "./SingleProducto";
 
 export class Router extends Component {
     state={
@@ -19,15 +21,27 @@ export class Router extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={()=>(
-              <Productos
-                productos={this.state.productos}
-              />
-          )} />
-          <Route exact path="/nosotros" component={Nosotros} />
-          <Route component={Error} />
-        </Switch>
+        <div className="contenedor">
+          <Header />
+            <Switch>
+              <Route exact path="/" render={()=>(
+                  <Productos
+                    productos={this.state.productos}
+                  />
+              )} />
+              <Route exact path="/nosotros" component={Nosotros} />
+              <Route exact path="/producto/:idproducto" render={(props)=>{
+                  let productoId = props.location.pathname.replace('/producto/','');
+                  return(
+                    <SingleProducto
+                      producto = {this.state.productos[productoId]}
+                    />
+                  )
+                }
+              } />
+              <Route component={Error} />
+            </Switch>
+        </div>
       </BrowserRouter>
     );
   }
