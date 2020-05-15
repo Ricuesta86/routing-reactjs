07fef6a113bc1db2afa9ac3b4dc095400e469aad
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2020-05-15 14:57:05 -0500

    Se creo la barra de navegacion

---
+++ src/componentes/Navegacion.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../css/navegacion.css'
+
+const Navegacion = () => {
+    return (
+        <nav className="navegacion" >
+            <Link to={'/nosotros'}>Nosotros</Link>
+            <Link to={'/productos'}>Productos</Link>
+            <Link to={'/contacto'}>Contacto</Link>            
+        </nav>
+    )
+}
+
+export default Navegacion
